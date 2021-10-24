135b0cf721ba8d39f409d41cfcc83163138f8837
Захар Чертухин <80054659+zaharchertuhin@users.noreply.github.com>
2021-10-24 13:26:57 +0300

    q
    q

--- Oreders_2/Load_Orders.cpp
+++ Oreders_2/Load_Orders.cpp
@@ -14,33 +14,35 @@ vector<Order> load_order(vector<Order>& orders) { //выгрузка заказов
 		while (!in.eof()) {
 			Order order;
 			string data;
+			bool key = true;
 			getline(in, data);
 			if (orders.size() == 0) order.id = atoi(data.c_str());
 			else {
 				for (auto j : orders) {
-					order.id = orders.size() + n;
+					//order.id = orders.size() + n;
 					if (atoi(data.c_str()) == j.id) {
-						order.id = orders.size() + n + 1;
+						//order.id = orders.size() + n + 1;
+						key = false;
 						break;
 					}
 
 				}
 
+			}if (key) {
+				getline(in, data);
+				order.Username = data;
+				getline(in, data);
+				order.description = data;
+				getline(in, data);
+				order.status = data;
+				getline(in, data);
+				order.price = atoi(data.c_str());
+				getline(in, data);
+				order.delete_status = atoi(data.c_str());
+				getline(in, data);
+				if (order.delete_status == false)order_list.push_back(order);
+				n++;
 			}
-			getline(in, data);
-			order.Username = data;
-			getline(in, data);
-			order.description = data;
-			getline(in, data);
-			order.status = data;
-			getline(in, data);
-			order.price = atoi(data.c_str());
-			getline(in, data);
-			order.delete_status = atoi(data.c_str());
-			getline(in, data);
-			if (order.delete_status == false)order_list.push_back(order);
-			n++;
-
 		}
 		order_list.pop_back();
 		in.close();
